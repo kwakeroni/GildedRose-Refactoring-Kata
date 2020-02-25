4408744dcc36a73e1922a9d307c104ad5b1e5794
kwakeroni <kwakeroni@gmail.com>
2020-02-25 14:29:30 +0100

    Fixed GildedRoseTest according to expected quality update

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,28 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Gilded Rose App")
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+    @Nested
+    @DisplayName("Update Quality")
+    class UpdateQualityTest {
 
+        @Test
+        @DisplayName("Degrades standard item with decrements of 1")
+        void testStandardItemDecrement() {
+            Item[] items = new Item[]{new Item("foo", 0, 0)};
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+
+            assertThat(app.items[0].name).isEqualTo("foo");
+            assertThat(app.items[0].sellIn).isEqualTo(-1);
+            assertThat(app.items[0].quality).isEqualTo(0);
+        }
+    }
 }
