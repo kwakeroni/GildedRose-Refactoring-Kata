563fb5d2f9bfed71e511d3958d46a4baf73d353f
kwakeroni <kwakeroni@gmail.com>
2020-02-25 18:24:27 +0100

    #8 - Added import from CSV functionality

--- src/test/java/com/gildedrose/io/CsvFormatTest.java
+++ src/test/java/com/gildedrose/io/CsvFormatTest.java
@@ -4,11 +4,45 @@ import com.gildedrose.Item;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.PrintWriter;
+import java.io.StringReader;
 import java.io.StringWriter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CsvFormatTest {
+
+    @Test
+    @DisplayName("Imports items in CSV format")
+    public void testCsvImport() throws Exception {
+        String input = "name, sellIn, quality\n" +
+                       "+5 Dexterity Vest, 10, 20\n" +
+                       "Aged Brie, 2, 0\n" +
+                       "Elixir of the Mongoose, 5, 7\n" +
+                       "\"Sulfuras, Hand of Ragnaros\", 0, 80\n" +
+                       "\"Sulfuras, Hand of Ragnaros\", -1, 80\n" +
+                       "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
+                       "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
+                       "Backstage passes to a TAFKAL80ETC concert, 5, 49\n" +
+                       "foo, 0, 0\n" +
+                       "Conjured Mana Cake, 3, 6";
+
+        Item[] items = new CsvFormat().parseItems(new StringReader(input));
+        assertThat(items)
+                .usingFieldByFieldElementComparator()
+                .containsExactly(
+                        new Item("+5 Dexterity Vest", 10, 20), //
+                        new Item("Aged Brie", 2, 0), //
+                        new Item("Elixir of the Mongoose", 5, 7), //
+                        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                        new Item("foo", 0, 0),
+                        new Item("Conjured Mana Cake", 3, 6));
+    }
+
 
     @Test
     @DisplayName("Outputs items in CSV format")
@@ -24,7 +58,7 @@ public class CsvFormatTest {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 new Item("foo", 0, 0),
                 new Item("Conjured Mana Cake", 3, 6)};
-
+        new CsvFormat().writeItems(items, new PrintWriter(System.out));
         StringWriter writer = new StringWriter();
         new CsvFormat().writeItems(items, writer);
 
